b6c792d198146ed29baccc196be9c53d97cdb09d
ywenhao <ywenhao2019@qq.com>
2023-04-16 00:56:26 +0800

    feat: 添加worker 并发

--- playground/vue/src/App.vue
+++ playground/vue/src/App.vue
@@ -3,10 +3,19 @@ import { getFileChunk } from '../../../src/utils/chunk'
 
 async function handleUploadFile(e: Event) {
   const file = (e.target as HTMLInputElement).files![0]
+  const chunkSize = 1024 * 1024 * 10
+  const workerDoCount = 10
+  const chunkCount = Math.ceil(file.size / chunkSize)
+  const promiseCount = Math.ceil(chunkCount / workerDoCount)
+
+  const fileChunk = (file: File) => getFileChunk({ file, chunkSize })
   console.time('chunk')
-  const fileChunk = await getFileChunk({ file, chunkSize: 1024 * 1024 * 10 })
+  const promiseList = Array(promiseCount).fill(false).map((_, i) => fileChunk(file.slice(i * chunkSize * workerDoCount, (i + 1) * chunkSize * workerDoCount) as File))
+
+  const res = await Promise.all(promiseList)
+  const fileChunks = res.map(v => v.fileChunks).reduce((pre, cur, index) => [...pre, ...(index ? cur.map(v => ({ ...v, index: v.index + pre.length })) : cur)], [])
+  console.log({ file, fileChunks })
   console.timeEnd('chunk')
-  console.log({ file, fileChunk })
 }
 </script>
 
