bb2fa3a94a294a194fbc6784f66936ed82ecc3df
actions.win <actions.win@foxmail.com>
2023-05-10 17:47:51 +0800

    feat: status修改

--- src/vueHooks.ts
+++ src/vueHooks.ts
@@ -19,11 +19,13 @@ export interface UseSliceUploadOptions extends Omit<SliceUploadOptions, 'file'> 
   request: UploadRequest
 }
 
+export type UploadStatus = 'ready' | 'uploading' | 'pause' | 'finish'
+
 export function useSliceUpload(options: UseSliceUploadOptions) {
   const progress = ref(0)
   const isFinish = ref(false)
   const chunks = ref<Chunks[]>([])
-  const status = ref<SliceUploadStatus>('ready')
+  const status = ref<UploadStatus>('ready')
 
   const instance = defineSliceUpload({ ...options, file: options.file.value! })
   options.preVerifyRequest && instance.setPreVerifyRequest(options.preVerifyRequest)
@@ -38,17 +40,18 @@ export function useSliceUpload(options: UseSliceUploadOptions) {
     progress.value = params.progress
     const { chunks: _chunks } = instance.getData()
     chunks.value = _chunks
-    status.value = instance.status
+    status.value = 'uploading'
   })
 
   instance.on('finish', (params) => {
-    status.value = instance.status
+    status.value = 'finish'
     isFinish.value = true
     options.onFinish?.(params)
   })
 
   instance.on('error', (error) => {
     options.onError?.(error)
+    status.value = 'pause'
   })
 
   const setRequest = (request: UploadRequest) => {
@@ -61,10 +64,12 @@ export function useSliceUpload(options: UseSliceUploadOptions) {
 
   const pause = () => {
     instance.pause()
+    status.value = 'pause'
   }
 
   const cancel = () => {
     instance.cancel()
+    status.value = 'ready'
   }
 
   const ajaxRequest = instance.ajaxRequest
